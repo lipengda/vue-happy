a296916b08de4a3421a8af2d3ca643a5060ea3be
Baan <980065665@qq.com>
2023-04-29 19:21:34 +0800

    element ui, router config.

--- vue3-project/src/main.ts
+++ vue3-project/src/main.ts
@@ -6,8 +6,17 @@ import router from './router'
 
 import './assets/main.css'
 
+import ElementPlus from 'element-plus' // 引入Element Plus 所需
+import 'element-plus/dist/index.css' // 引入Element Plus 所需
+import * as ElementPlusIconsVue from '@element-plus/icons-vue' // 引入Element Plus icon 所需
+
 const app = createApp(App)
 
+// 引入Element Plus icon 所需
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.use(ElementPlus) // 引入Element Plus 所需
 app.use(createPinia())
 app.use(router)
 
---
+++ vue3-project/src/views/Main.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import CommonAside from '../components/CommonAside.vue'
+</script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="180px">
+        <common-aside></common-aside>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.common-layout {
+  height: 100%;
+  & > .el-container {
+    height: 100%;
+    & > .el-aside {
+      height: 100%;
+      background: #545c64;
+    }
+    & > .el-container {
+      & > .el-header {
+        padding: 0%;
+      }
+    }
+  }
+}
+</style>
